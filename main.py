28f64d79713fbb20cf73ea4672521f686a59cd9e
Smirkyea <smirkyea@gmail.com>
2026-05-05 19:46:29 +0200

    feat(main, magnet): add physics, finish project

--- main.py
+++ main.py
@@ -2,33 +2,55 @@
 
 import pygame
 
-from models.magnet import Magnet
+from models.magnet import Magnet, update_magnets
+
+w_height = 800
+w_width = 600
+
+
+def clamp(value, min_value, max_value):
+    return max(min_value, min(value, max_value))
 
 
 class App:
     def __init__(self):
         pygame.init()
 
-        self.window = pygame.display.set_mode((800, 600))
+        self.window = pygame.display.set_mode((w_height, w_width))
         pygame.display.set_caption("Magnet Simulation")
 
         self.bg_color = (255, 255, 255)
 
-        self.magnet_s = Magnet(
-            x=255,
-            y=255,
-            size=50,
-            color=(0, 0, 255),
-            polarity="S",
-        )
-
-        self.magnet_n = Magnet(
-            x=255 + 50,
-            y=255,
-            size=50,
-            color=(255, 0, 0),
-            polarity="N",
-        )
+        self.magnets = [
+            Magnet(
+                x=250,
+                y=250,
+                size=50,
+                color=(255, 0, 0),
+                polarity="N",
+            ),
+            Magnet(
+                x=250 + 100,
+                y=250,
+                size=50,
+                color=(255, 0, 0),
+                polarity="N",
+            ),
+            Magnet(
+                x=250 + 100,
+                y=300,
+                size=50,
+                color=(0, 0, 255),
+                polarity="S",
+            ),
+            Magnet(
+                x=250 + 100,
+                y=-200,
+                size=50,
+                color=(0, 0, 255),
+                polarity="S",
+            ),
+        ]
 
         self.running = True
 
@@ -46,14 +68,17 @@ class App:
                 self.running = False
 
     def update(self):
-        # add the physics etc. here
-        pass
+        update_magnets(self.magnets)
+
+        for m in self.magnets:
+            m.x = clamp(m.x, 0, w_height - m.size)
+            m.y = clamp(m.y, 0, w_width - m.size)
 
     def render(self):
         self.window.fill(self.bg_color)
 
-        self.magnet_s.draw(self.window)
-        self.magnet_n.draw(self.window)
+        for m in self.magnets:
+            m.draw(self.window)
 
         pygame.display.flip()
 
